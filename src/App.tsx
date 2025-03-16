894c6df199d4302db8e6b845596add8ffe25ab7b
Chi Dung <chidung091@gmail.com>
2025-03-16 15:34:21 +0000

    add interactive answer buttons and update layout in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
 import "./App.css";
 
 function App() {
+  const text = "あ";
+  const answer = ["A", "B", "C"];
+  const checkAnswer = (answer: string) => {
+    const correct = "A";
+    if (answer === correct) {
+      console.log("correct");
+    } else {
+      console.log("incorrect");
+    }
+  };
   return (
     <>
-      <h1 className="bg-red-500">Hello</h1>
-      <button className="bg-red-500 text-black font-bold py-2 px-4 rounded">
-        Register
-      </button>
-      <button className="bg-green-500 hover:bg-green-300 text-black font-bold py-2 px-4 rounded">
-        Login
-      </button>
+      <div className="container mx-auto">
+        <h1 className="bg-red-500">Hello</h1>
+        <div className="flex justify-center">
+          <a className="border-green-300 border-2 w-50 h-50 text-center text-9xl">
+            {text}
+          </a>
+        </div>
+        <div aria-label="choice" className="flex justify-center">
+          <button
+            onClick={() => checkAnswer(answer[0])}
+            className="bg-green-500 hover:bg-green-200 text-white p-2 m-2 rounded w-20 h-20"
+          >
+            {answer[0]}
+          </button>
+          <button
+            onClick={() => checkAnswer(answer[1])}
+            className="bg-green-500 hover:bg-green-200 text-white p-2 m-2 rounded w-20 h-20"
+          >
+            {answer[1]}
+          </button>
+          <button
+            onClick={() => checkAnswer(answer[2])}
+            className="bg-green-500 hover:bg-green-200 text-white p-2 m-2 rounded w-20 h-20"
+          >
+            {answer[2]}
+          </button>
+        </div>
+      </div>
     </>
   );
 }
